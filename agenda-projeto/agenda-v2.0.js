1959e96f12a227299e7a81bca0334c582eb1d1e3
Lucas Barga <lucas.barga.ext@dasa.com.br>
2022-09-10 01:38:15 -0300

    up files

---
+++ agenda-projeto/agenda-v2.0.js
@@ -0,0 +1,12 @@
+// * ********************************************************
+// v1
+// criar agenda de contatos com as seguintes funcionalidades. BLA BLA BLA OBJETIVO.
+// cadastrar um contato e exibir todos os contatos.
+// os contatos devem ter nome, telefone e email.
+// salvar pelo menos 3 contatos e imprimir a lista
+// * ********************************************************
+// v2
+// criar uma funcao capaz de imprimir apenas 1 dos contatos
+// criar uma funcao capaz de deletar um contato
+// criar uma funcao capaz de atualizar um contato
+// * ********************************************************
