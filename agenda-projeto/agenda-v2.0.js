43d1fad5538764ca9b151197d25d7a0477ed5495
Lucas Barga <lucasmpbarga@gmail.com>
2022-09-16 22:50:11 -0300

    Update agenda-v2.0.js

--- agenda-projeto/agenda-v2.0.js
+++ agenda-projeto/agenda-v2.0.js
@@ -3,10 +3,11 @@
 // criar agenda de contatos com as seguintes funcionalidades. BLA BLA BLA OBJETIVO.
 // cadastrar um contato e exibir todos os contatos.
 // os contatos devem ter nome, telefone e email.
-// salvar pelo menos 3 contatos e imprimir a lista
+// salvar pelo menos 3 contatos e imprimir a lista.
+// Create and Read:All
 // * ********************************************************
 // v2
-// criar uma funcao capaz de imprimir apenas 1 dos contatos
-// criar uma funcao capaz de deletar um contato
-// criar uma funcao capaz de atualizar um contato
+// criar uma funcao capaz de imprimir apenas 1 dos contatos, pelo index.
+// criar uma funcao capaz de deletar um contato, pelo index.
+// Read:One and Delete
 // * ********************************************************
